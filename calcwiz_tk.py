3ae3ff221163f929a9c69ecc5a2d78cf4b2ce2d8
David Palacios <davidpal3c@gmail.com>
2023-11-30 21:59:43 -0700

    main init_test

--- calcwiz_tk.py
+++ calcwiz_tk.py
@@ -1,8 +1,14 @@
 
-import keyboard
 
-print("Press any freakin key")
-key = keyboard.wait()
-print(f"You pressed {key}")
+def main():
+    while True:
+        Welcome = input("Welcome to CalcWiz_Toolkit, press any key to continue...")
+        
+        if Welcome == "Y":
+            print("test")
+        else:
+            break
 
+if __name__ == "__main__":
+    main()
 
