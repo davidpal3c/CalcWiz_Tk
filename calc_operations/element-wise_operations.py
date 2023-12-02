7109dc827098b28b3bf211d0faece84d2dabae33
David Palacios <davidpal3c@gmail.com>
2023-12-01 20:52:43 -0700

    calc_operations update

---
+++ calc_operations/element-wise_operations.py
@@ -0,0 +1,34 @@
+
+# Addition
+
+
+
+# Substraction
+
+
+
+# Multiplication
+
+
+
+# Division ('/'):
+
+
+# Exponentiation ('**'):
+
+# Modulus ('%'):
+
+# Logical AND ('and'):
+
+# Logical OR ('or'):
+
+# Logical XOR (Exclusive OR):
+
+# Equality ('=='):
+
+# Inequality ('!='):
+
+"""These operations can be applied to other iterable data structures like NumPy 
+arrays or Pandas DataFrames in Python. The specific operations you choose 
+depend on the type of data and the problem you are trying to solve"""
+
--- calcwiz_tk.py
+++ calcwiz_tk.py
@@ -1,14 +1,20 @@
 
 
+def console_menu():
+    print("This is the main menu")
+
+
+
 def main():
     while True:
-        Welcome = input("Welcome to CalcWiz_Toolkit, press any key to continue...")
+        Welcome = input("Welcome to CalcWiz_Toolkit, press any key to continue...").upper().strip()
         
         if Welcome == "Y":
-            print("test")
+            console_menu()
         else:
             break
 
 if __name__ == "__main__":
     main()
+
 
