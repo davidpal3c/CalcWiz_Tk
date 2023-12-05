420e97f9615cd3afefbd6e75e38e01a321fbf66f
David Palacios <davidpal3c@gmail.com>
2023-12-05 11:30:25 -0700

    test_arith_op

--- tests/test_arith_op.py
+++ tests/test_arith_op.py
@@ -7,20 +7,6 @@ project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(project_root)
 
 # Use absolute import
-from calc_operations import ht_op
+from calc_operations import arithmetic_op as arith_op
 
 
-fruit_quantity = {"Mango": 2.4,   
-               "Strawyberry": 4,     
-               "Avocado": 3,         
-               "Blueberry":1.7}
-
-
-fruit_color = {"Mango": "f2c84b",   
-               "Strawyberry": "f2593a",     
-               "Avocado": "b3c752",         
-               "Blueberry":"2d56a8"}
-
-print(ht_op.get_keys(fruit_quantity))
-print(ht_op.get_max(fruit_quantity))
-print(ht_op.has_key(fruit_quantity, "Avocado")) # returns boolean
--- tests/tests_ht_op.py
+++ tests/tests_ht_op.py
@@ -1,13 +1,26 @@
 import os
 import sys
 
+# Add the project's root directory to the Python path
 current_dir = os.path.dirname(__file__)
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(project_root)
 
-from calc_operations import ht_operations as ht 
+# Use absolute import
+from calc_operations import ht_op
 
-h = {"Simon":23, "Elsa":28, "Tim":34, "Michelle":29}
+
+fruit_quantity = {"Mango": 2.4,   
+               "Strawyberry": 4,     
+               "Avocado": 3,         
+               "Blueberry":1.7}
 
 
-print(ht.get_keys(h))
+fruit_color = {"Mango": "f2c84b",   
+               "Strawyberry": "f2593a",     
+               "Avocado": "b3c752",         
+               "Blueberry":"2d56a8"}
+
+print(ht_op.get_keys(fruit_quantity))
+print(ht_op.get_max(fruit_quantity))
+print(ht_op.has_key(fruit_quantity, "Avocado")) # returns boolean
