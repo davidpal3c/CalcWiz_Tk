d59b06ecd8af4e2aa067c29518dd2820ae096071
David Palacios <davidpal3c@gmail.com>
2023-12-09 15:53:29 -0700

    data_handler init

---
+++ data_handler/data_handler.py
@@ -0,0 +1,12 @@
+
+
+class DataHandler:
+    def __init__(self):
+        # initialize as needed
+
+    def handle_numerical_input(self, user_input):
+        # Handle user input for numerical values
+        pass 
+
+
+    
