8a3b06ee49c481c884c1507b5f78ccfd138de9d0
David Palacios <davidpal3c@gmail.com>
2024-01-04 23:00:18 -0700

    is_palindrome bug fix

--- calc_operations/lst_op.py
+++ calc_operations/lst_op.py
@@ -57,7 +57,7 @@ def get_multiplied_values(lst, lst2):
 
 def is_palindrome(lst):
     # Check if all elements in the list are comparable (e.g., not None)
-    if any(element is None for element in my_list):
+    if any(element is None for element in lst):
         raise ValueError("List contains non-comparable elements (e.g., None)")  
     
     return lst == lst[::-1]
